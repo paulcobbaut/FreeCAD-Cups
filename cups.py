cda00019f87acb366f1b708f33ccf27ada59dad2
Paul Cobbaut <paul@cobbaut.be>
2022-11-25 21:41:12 +0100

    adding documentation, seems to work well already

--- cups.py
+++ cups.py
@@ -20,11 +20,18 @@ obj = doc.addObject("PartDesign::Body", "Body")
 def create_cup(ml, cup_name, dir_name):
 
     # create sketches that will contain the inner and outer circle of the cup
+    # the inner circle will form a cylinder with the exact millilitre contents
+    # the inner cylinder is later substracted from the outer cylinder to form a cup
     sketch_inner = doc.getObject('Body').newObject("Sketcher::SketchObject", "sketch_inner")
     sketch_outer = doc.getObject('Body').newObject("Sketcher::SketchObject", "sketch_outer")
     
-    # thickness of the solid part of the cups
-    wall = 4
+    # wall thickness of the solid part of the cups
+    if ml < 200:
+        wall = 3
+    elif ml < 1000:
+        wall = 4
+    else:
+        wall = 5
     
     # create inner cylinder
     radius = 10 * math.pow(ml/(2*math.pi),1/3)
@@ -42,7 +49,7 @@ def create_cup(ml, cup_name, dir_name):
     cup_outer.Profile = doc.getObject("sketch_outer")
     cup_outer.Length = height
     
-    # substract inner from outer cylinder
+    # substract inner from outer cylinder, we now have an exact ml cup
     cup = doc.addObject('Part::Cut', "cup")
     cup.Base = cup_outer
     cup.Tool = cup_inner
@@ -50,16 +57,20 @@ def create_cup(ml, cup_name, dir_name):
     # rotate 180 degrees so open end is on top in slicer
     Draft.rotate([doc.cup], 180.0, FreeCAD.Vector(0, 0, 0), axis=FreeCAD.Vector(0, -1, 0), copy=False)
 
-    # Create Shapestring
+    # Create Shapestring that contains the number of millilitre
+    # fontsize is set to half the radius, most numbers will fit the bottom of the cup
     ss=Draft.makeShapeString(String=ml,FontFile="/home/paul/FreeCAD models/cups_python/Vera.ttf",Size=(radius/2),Tracking=0.0)
-    # Vertical side placement
-    #ss.Placement = App.Placement(App.Vector(50,0,0),App.Rotation(App.Vector(0,1,0),90))
-    # Bottom side placement
-    ss.Placement = App.Placement(App.Vector(-(radius/2),0,-height),App.Rotation(App.Vector(1,0,0),0))
+    # Place at the bottom
+    # The X-value here is a weak attempt to center the shapestring on the bottom
+    # +5 is only significant for the very small cups
+    # -radius/1.1 is reasonable for large cups
+    ss.Placement = App.Placement(App.Vector(5-(radius/1.1),0,-height),App.Rotation(App.Vector(1,0,0),0))
     ss.Support=None
     Draft.autogroup(ss)
 
-    # Extrude ShapeString
+    # Extrude the ShapeString
+    # Later this extrude is subtracted from the cup
+    # Yes this means the contents is no longer exact, so don't fill the top 0.02mm or something :)
     extrude=doc.addObject('Part::Extrusion','extrude')
     extrude.Base = doc.getObject('ShapeString')
     extrude.LengthFwd = 0.5
@@ -72,6 +83,10 @@ def create_cup(ml, cup_name, dir_name):
     mcup.Tool = extrude
 
     # create rounded cup
+    # each cup exists in two versions:
+    # 1. mcup with sharp edges
+    # 2. fillet with rounded edges
+    # the bottom inside is NOT rounded
     doc.addObject("Part::Fillet","fillet")
     doc.fillet.Base = mcup
     __fillets__ = []
@@ -82,7 +97,7 @@ def create_cup(ml, cup_name, dir_name):
 
     doc.recompute()
 
-    # export .stl files for this cup and this rounded cup
+    # export .stl files for this sharp mcup and this rounded fillet
     export = []
     export.append(doc.getObject("mcup"))
     Mesh.export(export, u"/home/paul/FreeCAD models/cups_python/" + dir_name + "/sharp measuring cup " + cup_name + ".stl")
@@ -90,8 +105,9 @@ def create_cup(ml, cup_name, dir_name):
     export.append(doc.getObject("fillet"))
     Mesh.export(export, u"/home/paul/FreeCAD models/cups_python/" + dir_name + "/rounded measuring cup " + cup_name + ".stl")
 
-
-    # remove all objects
+    # remove all objects before the next for loop
+    # FreeCAD should be almost empty when this script finishes
+    # all cups are saved as .stl files
     doc.removeObject("extrude")
     doc.removeObject("ShapeString")
     doc.removeObject("fillet")
@@ -102,16 +118,14 @@ def create_cup(ml, cup_name, dir_name):
     doc.removeObject("sketch_inner")
     doc.removeObject("sketch_outer")
 
-"""
-for i in range(2):
-    ml = i + 5
+for i in range(25):
+    ml = i + 1
     create_cup(ml       , str(ml) + 'ml'                              , 'millilitre')
     create_cup(ml * 10  , str(ml) + 'cl' + '=' + str(ml * 10)   + 'ml', 'centilitre')
     create_cup(ml * 100 , str(ml) + 'dl' + '=' + str(ml * 100)  + 'ml', 'decilitre')
     create_cup(ml * 1000, str(ml) + 'l'  + '=' + str(ml * 1000) + 'ml', 'litre')
-"""
 
-create_cup(42, "42ml", "millilitre")
-create_cup(84, "84ml", "millilitre")
+# for testing individual cup creation
+#create_cup(42, "42ml", "millilitre")
 
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
